8a2621aba394130e8fe5128099e017fa293e468e
Aniket Mandal <aniketmandal9@gmail.com>
2019-12-02 23:52:37 +0530

    test routing with sample component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const pageOne = () => {
+  return <div>Page One</div>;
+};
+
+const pageTwo = () => {
+  return <div>Page Two</div>;
+};
 
 class App extends Component {
   render() {
-    return <div>Hi There!</div>;
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Route path="/" exact component={pageOne} />
+            <Route path="/pageTwo" component={pageTwo} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
