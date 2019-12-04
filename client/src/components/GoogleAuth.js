1df8749d06d2cb0c889edc748542bc5d9d359a83
Aniket Mandal <amandal@primesoft.net>
2019-12-04 14:41:32 +0530

    Added google oAuth clientId

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class GoogleAuth extends Component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId:
+          '659688161189-irfoqb93cptdhm32tbm9kg01ibel2mik.apps.googleusercontent.com',
+        scope: 'email'
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
