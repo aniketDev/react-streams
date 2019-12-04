fda1d8c6c329e576fdeffb0eb951275480a0a1f6
Aniket Mandal <amandal@primesoft.net>
2019-12-04 14:52:36 +0530

    show user signed in status

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
 
 class GoogleAuth extends Component {
+  state = {
+    isSignedIn: null
+  };
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
-      window.gapi.client.init({
-        clientId:
-          '659688161189-irfoqb93cptdhm32tbm9kg01ibel2mik.apps.googleusercontent.com',
-        scope: 'email'
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            '659688161189-irfoqb93cptdhm32tbm9kg01ibel2mik.apps.googleusercontent.com',
+          scope: 'email'
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
 
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I'm not sure if we logged in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>Logged in</div>;
+    } else {
+      return <div>Not logged in</div>;
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
