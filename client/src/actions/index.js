0b992a1e0bfb878e7e23fde7738513ab97f17239
Aniket Mandal <aniketmandal9@gmail.com>
2019-12-08 21:22:59 +0530

    created post request

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import { TYPES } from './types';
+import streams from '../apis/streams';
 
 export const signIn = userId => {
   return {
@@ -11,4 +12,8 @@ export const signOut = () => {
   return {
     type: TYPES.SIGN_OUT
   };
+};
+
+export const createStream = formValues => async dispatch => {
+  streams.post('/streams', formValues);
 };
